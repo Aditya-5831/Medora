73532b55e779b29820e01e4b9197e9d2875c57ae
Aditya-5831 <adityashiyale7219@gmail.com>
2025-09-19 19:52:34 +0530

    feat(auth): add sign in page with form and Google login

--- client/src/validations/auth.validation.ts
+++ client/src/validations/auth.validation.ts
@@ -1,5 +1,12 @@
 import z from "zod";
 
+export const SignInFormSchema = z.object({
+  email: z.email(),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+});
+
 export const SignUpFormSchema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
   email: z.email(),
@@ -8,4 +15,5 @@ export const SignUpFormSchema = z.object({
     .min(6, { message: "Password must be at least 6 characters long" }),
 });
 
+export type SignInFormValues = z.infer<typeof SignInFormSchema>;
 export type SignUpFormValues = z.infer<typeof SignUpFormSchema>;
