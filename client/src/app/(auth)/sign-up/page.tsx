d77c5c7bd06b3a7bdee6a26bcf9eac5c03498cba
Aditya-5831 <adityashiyale7219@gmail.com>
2025-09-19 19:42:14 +0530

    feat(auth): add signup page with form, Google button, and terms notice

---
+++ client/src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,63 @@
+import GoogleButton from "@/components/auth/GoogleButton";
+import SignUpForm from "@/components/auth/SignUpForm";
+import Footer from "@/components/layout/Footer";
+import Navbar from "@/components/layout/Navbar";
+import Link from "next/link";
+
+const SignUpPage = () => {
+  return (
+    <div className="mx-auto max-w-7xl">
+      <Navbar />
+
+      <div className="my-10 flex h-full w-full flex-col items-center px-5 sm:px-0">
+        <div className="flex w-full max-w-sm flex-col items-center justify-center gap-5">
+          <h1 className="text-center text-2xl font-bold text-pretty">
+            Create your Medora account
+          </h1>
+
+          {/* GOOGLE BUTTON */}
+          <GoogleButton />
+
+          <div className="flex items-center gap-2 before:h-px before:w-36 before:max-w-40 before:bg-gray-100 after:h-px after:w-36 after:max-w-40 after:bg-gray-100">
+            <span>or</span>
+          </div>
+
+          {/* SIGNUP FORM */}
+          <SignUpForm />
+
+          <p className="text-sm text-gray-500">
+            Already have an account?{" "}
+            <Link
+              href={"/sign-in"}
+              className="text-primary font-semibold underline"
+            >
+              Sign in
+            </Link>
+          </p>
+
+          <p className="mt-5 text-center text-sm text-pretty text-gray-500">
+            By signing up, you agree to our{" "}
+            <Link
+              href={"/terms"}
+              className="text-primary font-semibold underline"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href={"/terms"}
+              className="text-primary font-semibold underline"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default SignUpPage;
