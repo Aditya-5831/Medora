4598fb159aa5cca5aa826021a18dc228d0a193d3
Aditya-5831 <adityashiyale7219@gmail.com>
2025-09-25 20:42:24 +0530

    feat(auth, ui): add user authentication state, avatar generation, and dropdown menu integration in Navbar

--- client/src/components/layout/Navbar.tsx
+++ client/src/components/layout/Navbar.tsx
@@ -6,6 +6,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { buttonVariants } from "../ui/button";
+import { useAuthStore } from "@/stores/useAuthStore";
+import UserButton from "./UserButton";
 
 const NAV_LINKS = [
   {
@@ -37,23 +39,24 @@ const NAV_LINKS = [
 
 const Navbar = () => {
   const pathname = usePathname();
+  const { isAuthenticated, isLoadingUser } = useAuthStore((state) => state);
 
   return (
-    <nav className="w-full h-20 flex items-center justify-between px-6 md:px-12 shadow-sm">
+    <nav className="flex h-20 w-full items-center justify-between px-6 shadow-sm md:px-12">
       {/* LOGO */}
       <div className="flex items-center gap-1">
         <Image src={"/logo.svg"} width={25} height={25} alt="logo" />
-        <span className="hidden sm:block font-bold">MEDORA</span>
+        <span className="hidden font-bold sm:block">MEDORA</span>
       </div>
 
       {/* DESKTOP LINKS */}
-      <div className="hidden sm:flex items-center gap-8">
+      <div className="hidden items-center gap-8 sm:flex">
         {NAV_LINKS.map((link) => (
           <Link
             key={link.href}
             href={"/"}
             className={cn(
-              "font-medium text-sm text-gray-700 hover:text-primary transition-colors",
+              "hover:text-primary text-sm font-medium text-gray-700 transition-colors",
               {
                 "text-primary font-bold": pathname === link.href,
               },
@@ -65,39 +68,45 @@ const Navbar = () => {
       </div>
 
       {/* BUTTONS SECTION */}
-      <div className="hidden md:flex items-center gap-3">
-        <Link
-          href={"/sign-up"}
-          className={buttonVariants({
-            size: "sm",
-            className: "text-xs group",
-          })}
-        >
-          Sign up
-          <ArrowRight className="size-4 group-hover:translate-x-1 transition-all duration-200" />
-        </Link>
+      {isLoadingUser ? (
+        <div />
+      ) : isAuthenticated ? (
+        <UserButton />
+      ) : (
+        <div className="hidden items-center gap-3 md:flex">
+          <Link
+            href={"/sign-up"}
+            className={buttonVariants({
+              size: "sm",
+              className: "group text-xs",
+            })}
+          >
+            Sign up
+            <ArrowRight className="size-4 transition-all duration-200 group-hover:translate-x-1" />
+          </Link>
 
-        <div className="h-8 w-px bg-gray-200" />
+          <div className="h-8 w-px bg-gray-200" />
 
-        <Link
-          href={"/sign-up"}
-          className={buttonVariants({
-            size: "sm",
-            className: "text-xs",
-            variant: "ghost",
-          })}
-        >
-          Sign in
-        </Link>
-      </div>
+          <Link
+            href={"/sign-up"}
+            className={buttonVariants({
+              size: "sm",
+              className: "text-xs",
+              variant: "ghost",
+            })}
+          >
+            Sign in
+          </Link>
+        </div>
+      )}
 
       {/* MOBILE MENU */}
-      <div className="sm:hidden absolute bottom-5 left-0 flex items-center justify-around w-full border-t pt-5 rounded-t-xl">
+      <div className="absolute bottom-5 left-0 flex w-full items-center justify-around rounded-t-xl border-t pt-5 sm:hidden">
         {NAV_LINKS.map((link) => (
           <Link
             key={link.href}
             href={link.href}
-            className={cn("py-2 text-gray-700 font-medium hover:text-black", {
+            className={cn("py-2 font-medium text-gray-700 hover:text-black", {
               "text-primary": pathname === link.href,
             })}
           >
@@ -110,16 +119,16 @@ const Navbar = () => {
       </div>
 
       {/* MOBILE BUTTON SECTION */}
-      <div className="sm:hidden items-center gap-3">
+      <div className="items-center gap-3 sm:hidden">
         <Link
           href={"/sign-up"}
           className={buttonVariants({
             size: "sm",
-            className: "text-xs group",
+            className: "group text-xs",
           })}
         >
           Sign up
-          <ArrowRight className="size-4 group-hover:translate-x-1 transition-all duration-200" />
+          <ArrowRight className="size-4 transition-all duration-200 group-hover:translate-x-1" />
         </Link>
       </div>
     </nav>
